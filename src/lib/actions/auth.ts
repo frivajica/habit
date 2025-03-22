60358bf9d6e73bcc3c5c062ab2ca85753fa63d34
Fran <franivan.j.c@gmail.com>
2025-03-22 13:43:45 -0600

    Debug bump.

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -1,25 +1,25 @@
 'use server'
 
 import { signIn, signOut } from "@/auth"
-// import { db } from "@/db"
-// import { users } from "@/db/schema/users"
+import { db } from "@/db"
+import { users } from "@/db/schema/users"
 
 export const login = async () => {
 	await signIn('google', { redirectTo: '/' })
 }
 export const logout = async () => {
 
-	// const newUser = await db
-	// .insert(users)
-	// .values({
-	// 	firstName: 'Fran',
-	// 	lastName: 'Jim',
-	// 	email: 'franjim@gmail.com',
-	// 	avatar: 'someteest',
-	// })
-	// .onConflictDoNothing({ target: users.email })
-	// .returning();
-	// console.log('newUser ---------->', newUser);
+	const newUser = await db
+	.insert(users)
+	.values({
+		firstName: 'Fran',
+		lastName: 'Jim',
+		email: 'franjim@gmail.com',
+		avatar: 'someteest',
+	})
+	.onConflictDoNothing({ target: users.email })
+	.returning();
+	console.log('newUser ---------->', newUser);
 
 	await signOut({ redirectTo: '/' })
 	
